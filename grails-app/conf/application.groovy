59bf6584ef1f6ac25fef7691ce08c3097d756ec1
hartmut <hartmut@192.168.2.10>
2016-04-14 22:14:37 +0200

    Upgraded application.goovy to appication.yml

--- grails-app/conf/application.groovy
+++
@@ -1,6 +0,0 @@
-grails.resources.modules = {
-}
-grails.databinding.dateFormats = [
-"yyyy-MM-dd'T'HH:mm:ss.SSSXXX", "yyyy-MM-dd'T'HH:mm:ss.SSSZ" ,'yyyy-MM-dd HH:mm:ss.S','yyyy-MM-dd HH:mm', 'yyyy/MM/dd HH:mm','MMddyyyy',
-'yyyy/MM/dd hh:mm TT'
-]
